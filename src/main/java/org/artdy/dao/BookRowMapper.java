2039aa3e46fb9a0573d069f0c44a1382b50f1638
Артём <gartdy@gmail.com>
2022-11-12 22:30:13 +0300

    Add row mapper for book

--- src/main/java/org/artdy/dao/BookRowMapper.java
+++ src/main/java/org/artdy/dao/BookRowMapper.java
@@ -1,2 +1,21 @@
-package org.artdy.dao;public class BookRowMapper {
+package org.artdy.dao;
+
+import org.artdy.models.Book;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookRowMapper implements RowMapper {
+    @Override
+    public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Book book = new Book(
+                rs.getInt("book_id"),
+                rs.getString("title"),
+                rs.getString("book_id"),
+                rs.getInt("publication_year"),
+                rs.getInt("person_id")
+                );
+        return null;
+    }
 }
